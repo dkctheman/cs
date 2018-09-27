b9d9dc6efb44d4caeef0ddc8dc961fbcbbb9254d
Dinesh KC <dineshkc@outlook.com>
2018-09-26 23:08:24 -0700

    v1.1

--- sorting/sorting/Program.cs
+++ sorting/sorting/Program.cs
@@ -6,7 +6,30 @@ namespace sorting
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            const int MIN = 0;
+            const int MAX = 100;
+            int length = 10;
+            int[] array = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = rand.Next(MIN, MAX);
+
+            }
+
+            bubbleSort bs = new bubbleSort(array);
+            bs.sort();
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("{0}", array[i]);
+            }
+            
+
             Console.WriteLine("Hello World!");
         }
+
+
     }
 }
---
+++ sorting/sorting/bubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sorting
+{
+    class bubbleSort
+    {
+        private int[] array;
+        public bubbleSort(int[] array)
+        {
+            this.array = array;
+        }
+
+        public void sort()
+        {
+            int length = array.Length-1;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (array[i] > array[i+1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i+1];
+                        array[i + 1] = temp;
+
+                    }
+                }
+            }
+        }
+
+    }
+}
