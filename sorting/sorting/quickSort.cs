abd1e7efd8340031a24e113947e851eea8590260
=Dinesh KC <=dineshkc@outlook.com>
2018-09-28 14:52:07 -0700

    v1.4

---
+++ sorting/sorting/quickSort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sorting
+{
+    class quickSort
+    {
+        private int[] array;
+
+        public quickSort(int[] array)
+        {
+            this.array = array;
+        }
+
+
+    }
+}
