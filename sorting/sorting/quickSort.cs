3cb773a0bdcfc8781f826dfeeca5dea2945c88e9
Dinesh Kc <dineshkc@outlook.com>
2018-10-01 07:25:17 -0700

    v2.0

--- sorting/sorting/quickSort.cs
+++ sorting/sorting/quickSort.cs
@@ -13,6 +13,30 @@ namespace sorting
             this.array = array;
         }
 
+        public void merge(int s1, int e1, int s2, int e2)
+        {
+
+            if (array[e1] < array[s2])
+            {
+                return;
+            }
+
+            for (int i = s2; i <= e2; i++)
+            {
+                for (int j = i; j > s1; j--)
+                {
+                    if (array[j-1] > array[j])
+                    {
+                        int temp = array[j-1];
+                        array[j-1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+
+        }
+
 
     }
 }
