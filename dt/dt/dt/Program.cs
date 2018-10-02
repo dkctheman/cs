babdf11130dfbc25f6d872a6e41c26f0911ecba8
=Dinesh KC <=dineshkc@outlook.com>
2018-10-02 16:17:40 -0700

    dt added

---
+++ dt/dt/dt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace dt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            list l = new list();
+            int length = 10;
+            int min = 0;
+            int max = 100;
+            int[] array = new int[length];
+            Random r = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int temp = r.Next(min, max);
+                l.add(temp);
+                array[i] = temp;
+            }
+
+            l.print();
+            l.remove(array[4]);
+            l.print();
+            l.add(88);
+            l.print();
+            
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ dt/dt/dt/list.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dt
+{
+    class list
+    {
+        private int a;
+        list next;
+
+        public list()
+        {
+            this.next = null;
+
+        }
+
+        public list(int a)
+        {
+            list temp = new list();
+            temp.a = a;
+            temp.next = null;
+            this.next = temp;
+        }
+
+
+        public void add(int a)
+        {
+            list temp = new list();
+            temp.a = a;
+            temp.next = null;
+            list current = this;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            current.next = temp;
+        }
+
+        public void remove (int a)
+        {
+            list current = this;
+
+            while (current.next != null)
+            {
+                if(current.next.a == a)
+                {
+                    current.next = current.next.next;
+                    return;
+                }
+
+                current = current.next;
+            }
+
+        }
+
+        public void print()
+        {
+            list current = this.next;
+            while (current != null)
+            {
+                Console.WriteLine("{0}",current.a);
+                current = current.next;
+            }
+            Console.WriteLine("---------------");
+        }
+
+    }
+}
